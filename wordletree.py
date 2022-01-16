477d4b125f3a5f84e10974ac353d6ef48db4e0db
Greg Baker <bakergo@gmail.com>
2022-01-16 14:29:37 -0800

    Rough optimization for first guess of wordle
    Basically, count the maximum remaining words
    The next steps would be to iterate over this and build a
    branch-and-bound guesser.

---
+++ wordletree.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+WORDLIST=set()
+with open('words.txt', 'r') as f:
+    for line in f:
+        WORDLIST.add(line.lower().strip())
+
+
+def posmap(string):
+    return {i:string[i] for i in range(len(string))}
+
+def score(guess, against):
+    '''Return a tuple of 5 integers representing the score of this word against the list'''
+    r = [0] * 5
+    guessletters = posmap(guess)
+    againstletters = posmap(against)
+
+    for k,v in guessletters.copy().items():
+        if v == againstletters[k]:
+            r[k] = 2
+            del againstletters[k]
+            del guessletters[k]
+
+    for k,v in guessletters.items():
+        for kb, vb in againstletters.items():
+            if v == vb:
+                del againstletters[kb]
+                r[k] = 1
+                break
+
+    return tuple(r)
+
+def main():
+    guessmin = {}
+    for word in WORDLIST:
+        wordscores = {}
+        for word2 in WORDLIST:
+            wordscore = score(word, word2)
+            if wordscore in wordscores:
+                wordscores[wordscore] += 1
+            else:
+                wordscores[wordscore] = 1
+            print('%s : %s -> %s'% (word, word2, score(word, word2)))
+        maxv = max(wordscores.values())
+        guessmin[word] = maxv
+    minimum = min(guessmin.values())
+    for k,v in guessmin.items():
+        if v == minimum:
+            print('%s is a good choice (score: %d)' % (k, v))
+
+
+main()
+
