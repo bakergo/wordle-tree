5d17a5d0bcd239a1f84dcec7dcc03bc04fe318c9
Greg Baker <bakergo@gmail.com>
2022-01-25 01:44:44 -0800

    Several big additions to the script
    * Add dependency on a progress bar implementation for python; tqdm.
    * Add a couple early termination points based on estimating the size of
      the guess tree; you need one guess per 3**5 magnitude at least, so
      bail when that threshold cannot be met.
    * Prefer to test the "best" guesses from earlier in the iteration. These
      tend to be the more promising guesses anyway, so biasing the search
      toward them is productive. Not retained through recursive layers, but
      should be.
    
    Overall this is magnitudes faster than previously, which was necessary.

--- wordletree.py
+++ wordletree.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import tqdm
+import random
+import math
+
 GUESSES=set()
 ANSWERS=set()
 
@@ -11,7 +15,6 @@ with open('answers.txt', 'r') as f:
         ANSWERS.add(line.lower().strip())
 
 GUESSES.update(ANSWERS)
-
 
 def posmap(string):
     return {i:string[i] for i in range(len(string))}
@@ -37,11 +40,115 @@ def score(guess, against):
 
     return tuple(r)
 
+def bucket(guess, answers):
+    buckets = {}
+    for word in answers:
+        wordscore = score(guess, word)
+        if wordscore in buckets:
+            buckets[wordscore] += [word]
+        else:
+            buckets[wordscore] = [word]
+    return buckets
+
+
+def filter_words(wordscore, guess, words):
+    return [word for word in words if score(guess, word) == wordscore]
+
+def get_stacky_best_guess(guesses, answers, hard_mode, beat, top_level=False, position=0, postfix=""):
+    # Only one answer exists. Guess it
+    if len(answers) == 1:
+        answer = next(iter(answers))
+        return 1, answer, {answer:None}
+    # Beat is too small; we can't get it.
+    if beat < math.ceil(math.log(len(answers), 243)) + 1:
+        return beat+1, None, None
+    best_guess_depth = beat
+    best_guess = None
+    best_tree = {}
+
+    t=tqdm.tqdm(total=len(guesses), position=position, unit="guess", leave=False, postfix=postfix, ncols=120, mininterval=1)
+    guess_stack=random.sample(list(guesses), k=len(guesses))
+    visited=set()
+    while guess_stack:
+        if best_guess_depth <= math.ceil(math.log(len(answers), 243)) and best_guess:
+            # Theoretically impossible to beat this guess.
+            break
+        guess = guess_stack.pop()
+        if guess in visited:
+            continue
+        t.update(n=1)
+        visited.add(guess)
+        guess_tree = {}
+        guess_depth = float('-inf')
+        distr = bucket(guess, answers)
+        for score, next_words in distr.items():
+            if hard_mode:
+                next_guesses = filter_words(score, guess, guesses)
+            else:
+                next_guesses = guesses
+            depth, next_guess, tree = get_stacky_best_guess(next_guesses, next_words, hard_mode, best_guess_depth-1, position=position+1, postfix=guess)
+            if next_guess is None:
+                # Did not solve it.
+                guess_depth = best_guess_depth + 1
+                break
+            if depth > guess_depth:
+                guess_depth = depth
+            if guess_depth > best_guess_depth:
+                break
+            elif next_guess not in visited:
+                guess_stack.append(next_guess)
+            guess_tree[score] = (next_guess, tree)
+        if guess_depth < best_guess_depth:
+            best_guess_depth = guess_depth
+            best_guess = guess
+            best_tree = guess_tree
+    return best_guess_depth + 1, best_guess, best_tree
+
+def get_best_guess(guesses, answers, hard_mode, beat, top_level=False, position=0, postfix=""):
+    # Only one answer exists. Guess it
+    if len(answers) == 1:
+        answer = next(iter(answers))
+        return 1, answer, {answer:None}
+    # Beat is 0 or 1 and we can't get it in 1.
+    if beat < 2:
+        return 2, None, None
+    best_guess_depth = beat
+    best_guess = None
+    best_tree = {}
+    for guess in tqdm.tqdm(guesses, position=position, unit="guess", leave=False, postfix=postfix):
+        # This guess leads to a next-guess win
+        if best_guess_depth <= 1:
+            break
+        guess_tree = {}
+        guess_depth = float('-inf')
+        distr = bucket(guess, answers)
+        for score, next_words in distr.items():
+            if hard_mode:
+                next_guesses = filter_words(score, guess, guesses)
+            else:
+                next_guesses = guesses
+            depth, next_guess, tree = get_best_guess(next_guesses, next_words, hard_mode, best_guess_depth-1, position=position+1, postfix=guess)
+            if next_guess is None:
+                # Did not solve it.
+                guess_depth = best_guess_depth + 1
+                break
+            if depth > guess_depth:
+                guess_depth = depth
+            if guess_depth > best_guess_depth:
+                break
+            guess_tree[score] = (next_guess, tree)
+        if guess_depth < best_guess_depth:
+            best_guess_depth = guess_depth
+            best_guess = guess
+            best_tree = guess_tree
+    return best_guess_depth + 1, best_guess, best_tree
+
 
 def next_guess(guesses, answers):
     guessmin = {}
     for word in guesses:
         wordscores = {}
+
         for word2 in answers:
             wordscore = score(word, word2)
             if wordscore in wordscores:
@@ -102,9 +209,17 @@ def print_hist(hist, hard_mode=False):
         if k in hist:
             print("%-4s: %d" % ("fail" if k==-1 else str(k), hist[k]))
 
-def main():
+def solve_wordle():
+    depth,guess,tree = get_stacky_best_guess(ANSWERS, ANSWERS, hard_mode=False, beat=5, top_level=True)
+    print("A solution exists for wordle in %d guesses, with %s as the start." % (depth, guess))
+    print("The guess tree follows:")
+    print(tree)
+    print("A solution exists for wordle in %d guesses, with %s as the start." % (depth, guess))
+
+
+def do_histograms():
     easy_hist, easy_beaten_by = make_hist(False)
-    hard_hist, hard_beaten_by = make_hist(False)
+    hard_hist, hard_beaten_by = make_hist(True)
     if easy_beaten_by:
         print("Beaten by these words on easy:")
         for word in easy_beaten_by:
@@ -116,6 +231,10 @@ def main():
             print(word)
     print_hist(hard_hist, hard_mode=True)
     print("fin")
+
+def main():
+    # do_histograms()
+    solve_wordle()
 
 main()
 
