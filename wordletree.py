36ef5175cb941a11710314548d98b3b09136dc56
Greg Baker <bakergo@gmail.com>
2024-04-20 21:59:37 -0700

    Delete unused code and some other things
    Fixes the score when an exact match is found - GGGGG is score 0
    Deletes a bunch of unused stuff

--- wordletree.py
+++ wordletree.py
@@ -47,11 +47,11 @@ def precompute_buckets():
         BUCKETS[guess] = _bucket(guess, ANSWERS)
 
 
-def bucket(guess, answers):
+def bucket(guess, answers: set):
     if not BUCKETS:
         precompute_buckets()
     buckets = BUCKETS[guess]
-    aset = set(answers)
+    aset = answers
     ret = {}
     for s, w in buckets.items():
         r = w & aset
@@ -71,7 +71,7 @@ def _bucket(guess, answers):
     return buckets
 
 
-def get_any_better_guess(guesses, answers, beat, max_depth=6, postfix="root"):
+def get_any_better_guess(guesses, answers: set, beat, max_depth=6, postfix="root"):
     """Returns a tree with the minimum total guesses required."""
     # Only one answer exists. Guess it
     if len(answers) == 1:
@@ -98,17 +98,17 @@ def get_any_better_guess(guesses, answers, beat, max_depth=6, postfix="root"):
     guess_order = guess_order[:len(guesses)//100]
 
     # guess_stack=random.sample(list(guesses), k=len(guesses))
-    for (weight, guess, distr) in tqdm.tqdm(guess_order, unit="bucket", leave=False, postfix=postfix, ncols=120):
+    for (weight, guess, distr) in tqdm.tqdm(guess_order, unit="guess", leave=False, postfix=postfix, ncols=120):
         guess_tree = {}
         guess_total = 0.
         guessed_count = 0
         len_distr = len(distr)
         for (score, next_words) in tqdm.tqdm(distr.items(),
-                      total=len(distr),
-                      unit="guess",
-                      leave=False,
-                      postfix=guess,
-                                           ncols=120):
+                                             total=len(distr),
+                                             unit="bucket",
+                                             leave=False,
+                                             postfix=guess,
+                                             ncols=120):
             next_guesses = guesses
             # Trim down the guess budget for the subcall based on the number of items in this
             # bucket.
@@ -121,7 +121,10 @@ def get_any_better_guess(guesses, answers, beat, max_depth=6, postfix="root"):
                                      -guess_total)
             # Use no more than max_depth - 1
             budget = min(total_budget_for_kids, max_depth - 1)
-            next_guess_average, next_guess, tree = get_any_better_guess(next_guesses, next_words, budget, max_depth=max_depth-1, postfix=score)
+            if score == 'GGGGG':
+                next_guess_average, next_guess, tree = (0, guess, None)
+            else:
+                next_guess_average, next_guess, tree = get_any_better_guess(next_guesses, next_words, budget, max_depth=max_depth-1, postfix=score)
             guessed_count += 1
             if next_guess is None:
                 # This bucket is unsolveable in the budget and depth
@@ -138,76 +141,10 @@ def get_any_better_guess(guesses, answers, beat, max_depth=6, postfix="root"):
             upper_bound = float(guess_total) / len_distr
             best_guess = guess
             best_tree = guess_tree
-        if upper_bound <= 1 and best_guess:
+        if upper_bound <= (1 + (len_distr-1)/len_distr) and best_guess:
             # Optimal
             break
     return upper_bound + 1, best_guess, best_tree
-
-
-def next_guess(guesses, answers):
-    guessmin = {}
-    for word in guesses:
-        wordscores = {}
-
-        for word2 in answers:
-            wordscore = score(word, word2)
-            if wordscore in wordscores:
-                wordscores[wordscore] += [word2]
-            else:
-                wordscores[wordscore] = [word2]
-            # print('%s : %s -> %s'% (word, word2, wordscore))
-        maxv = max((tuple((len(x), 1 if word in ANSWERS else 0)) for x in wordscores.values()))
-        guessmin[word] = maxv
-    minimum = min(guessmin.values())
-    for k,v in guessmin.items():
-        if v == minimum:
-            # print('%s is a good choice (score: %d)' % (k, v))
-            return k
-
-def guess_word_path(answer, hard_mode=False):
-    valid_guesses = GUESSES
-    valid_answers = ANSWERS
-    # hard code it to save time.
-    guess = 'aesir'
-    # guess = next_guess(valid_guesses, valid_answers)
-    guesses = 1
-    while True:
-        next_score = score(guess, answer)
-        print("Guess %d: %s %s" % (guesses, guess, next_score))
-        if next_score == (2,2,2,2,2):
-            print("%s is the word. Got it in %d" % (guess, guesses))
-            return guesses
-        elif guesses >= 6:
-            print("Couldn't get it sadge")
-            return -1
-        guesses += 1
-        valid_answers = [word for word in valid_answers if score(guess, word) == next_score]
-        if hard_mode:
-            valid_guesses = [word for word in valid_guesses if score(guess, word) == next_score]
-        if len(valid_answers) == 1:
-            guess = valid_answers[0]
-        else:
-            guess = next_guess(valid_guesses, valid_answers)
-
-
-def make_hist(hard_mode=False):
-    beaten_by = set()
-    hist = dict()
-    for x in range(-1,7):
-        hist[x] = 0
-    for word in ANSWERS:
-        guesses = guess_word_path(word, hard_mode=hard_mode)
-        hist[guesses] += 1
-        if guesses < 0:
-            beaten_by.add(word)
-    return hist, beaten_by
-
-
-def print_hist(hist, hard_mode=False):
-    print("%s mode histogram" % ("Hard" if hard_mode else "Easy"))
-    for k in range(-1, 7):
-        if k in hist:
-            print("%-4s: %d" % ("fail" if k==-1 else str(k), hist[k]))
 
 
 def solve_wordle():
@@ -218,25 +155,8 @@ def solve_wordle():
     print("A solution exists for wordle in %f guesses, with %s as the start." % (depth, guess))
 
 
-def do_histograms():
-    easy_hist, easy_beaten_by = make_hist(False)
-    hard_hist, hard_beaten_by = make_hist(True)
-    if easy_beaten_by:
-        print("Beaten by these words on easy:")
-        for word in easy_beaten_by:
-            print(word)
-    print_hist(easy_hist, hard_mode=False)
-    if hard_beaten_by:
-        print("Beaten by these words on hard:")
-        for word in hard_beaten_by:
-            print(word)
-    print_hist(hard_hist, hard_mode=True)
-    print("fin")
-
 def main():
-    # do_histograms()
     solve_wordle()
-    # any_wordle()
 
 main()
 
